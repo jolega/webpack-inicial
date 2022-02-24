e3b80bb612955355ee495b10e5eab0ec858f3cc6
jolega <alonso_juancho@hotmail.com>
2022-02-24 09:14:19 -0500

    creando archivos de configuracion de webpack

---
+++ src/index.js
@@ -0,0 +1,5 @@
+
+import { saludar } from './js/componentes.js'
+
+const nombre = 'Johan Garcia'
+saludar(nombre)
---
+++ src/js/componentes.js
@@ -0,0 +1,14 @@
+
+
+
+
+export const saludar = (nombre) => {
+
+    console.log('Creado Etiqueta h1');
+    
+    const h1= document.createElement('h1');
+    h1.innerText = `Hola, ${nombre}`;
+
+    document.body.append(h1);
+
+}
